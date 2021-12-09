0a06c45888864c5c3f6970296466fffffe5fe0d2
Aslihan Akbiyik <48156075+aslihana@users.noreply.github.com>
2021-12-09 22:53:56 +0300

    second commit

--- authServer.js
+++ authServer.js
@@ -19,7 +19,7 @@ const posts = [
 
 
 app.get('/posts', authenticateToken, (req, res) => {
-	res.json(posts.filter(post => post.username === req.user.name));
+	const refreshToken = req.body.token
 })
 
 app.get('/login', (req, res) => {
@@ -28,11 +28,18 @@ app.get('/login', (req, res) => {
 	const username = req.body.username
 	const user = { name: username }
 
-	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-	res.json({ accessToken: accessToken})
+	const accessToken = generateAccessToken(user);
+	const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+
+	res.json({ accessToken: accessToken, refreshToken: refreshToken})
 })
 
-function authenticateToken(req, res, next) {
+function generateAccessToken(user) {
+	return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10m'})
+
+}
+
+/*function authenticateToken(req, res, next) {
 	const authHeader = req.headers['authorization']
 	const token = authHeader && authHeader.split(' ')[1]
 	if(token == null) return res.sendStatus(401)
@@ -43,6 +50,6 @@ function authenticateToken(req, res, next) {
 		  next()
 		})
 	
-}
+} */
 
 app.listen(4000);
--- server.js
+++ server.js
@@ -22,27 +22,6 @@ app.get('/posts', authenticateToken, (req, res) => {
 	res.json(posts.filter(post => post.username === req.user.name));
 })
 
-app.get('/login', (req, res) => {
-	//Authenticate the user
 
-	const username = req.body.username
-	const user = { name: username }
-
-	const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-	res.json({ accessToken: accessToken})
-})
-
-function authenticateToken(req, res, next) {
-	const authHeader = req.headers['authorization']
-	const token = authHeader && authHeader.split(' ')[1]
-	if(token == null) return res.sendStatus(401)
-
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,user) => {
-		  if(err) return res.sendStatus(403)
-		  	req.user = user 
-		  next()
-		})
-	
-}
 
 app.listen(3000);
